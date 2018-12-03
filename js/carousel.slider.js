ae8f98a312f69481b2099caaa65b564464784920
San San Thi <sansanthi1996@gmail.com>
2018-12-03 22:45:49 +0630

    home page

---
+++ js/carousel.slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+   })
+   $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
